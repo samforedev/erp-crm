c6f522fd807c5d9fd75dbd9b75f9657c69575199
Samuel Forero <samuelf.dev@outlook.com>
2025-11-09 20:35:44 -0500

    Ajuste para construccion en Docker v0.0.1

--- auth-service/src/main/java/com/sam/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/sam/authservice/AuthServiceApplication.java
@@ -1,6 +1,5 @@
 package com.sam.authservice;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,13 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
-        LoadConfiguration();
         SpringApplication.run(AuthServiceApplication.class, args);
-    }
-
-    public static void LoadConfiguration() {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(x -> System.setProperty(x.getKey(), x.getValue()));
     }
 
 }
--- insurance-service/src/main/java/com/sam/insuranceservice/InsuranceServiceApplication.java
+++ insurance-service/src/main/java/com/sam/insuranceservice/InsuranceServiceApplication.java
@@ -1,6 +1,5 @@
 package com.sam.insuranceservice;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,13 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InsuranceServiceApplication {
 
     public static void main(String[] args) {
-        LoadConfiguration();
         SpringApplication.run(InsuranceServiceApplication.class, args);
-    }
-
-    public static void LoadConfiguration() {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(x -> System.setProperty(x.getKey(), x.getValue()));
     }
 
 }
